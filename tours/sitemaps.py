37bfd6d19b7952967d168da957cdc00b7bf642c9
JDaxmaut <kakojtoktotovic@gmail.com>
2026-07-06 16:11:20 +0300

    о ведьмы ведьмы

--- tours/sitemaps.py
+++ tours/sitemaps.py
@@ -7,6 +7,7 @@ from wagtail.contrib.sitemaps.views import sitemap as wagtail_sitemap
 def sitemap(request):
     response = wagtail_sitemap(request)
     if settings.WAGTAILADMIN_BASE_URL and response.status_code == 200:
+        response.render()
         content = response.content.decode("utf-8")
         content = re.sub(
             r"https?://[^/]+(?::\d+)?",
