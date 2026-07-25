95172e22ae092b3d1fb9f319605814551358cd6b
JDaxmaut <kakojtoktotovic@gmail.com>
2026-07-25 16:43:17 +0300

    Add collectstatic to entrypoint.sh for runtime static collection

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 mkdir -p /app/data
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 exec gunicorn tochka.wsgi:application \
     --bind 0.0.0.0:8002 \
     --workers 2 \
